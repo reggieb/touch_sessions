e21388c8bbf86e8d15eaa264f70ffdc820a9f48d
Rob Nichols <rob@nicholshayes.co.uk>
2019-05-06 14:50:59 +0100

    Add navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def nav_link_to(text, path)
+    link = link_to_unless_current text, path, class: 'nav-link' do
+      link_to text, '#', class: 'nav-link'
+    end
+    classes = ['nav-item']
+    classes << 'active' if current_page?(path)
+    content_tag :li, link, class: classes.join(' ')
+  end
 end
